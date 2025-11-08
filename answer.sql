54590bb020957628f99984c70996a648af547950
Aseel A. Al-thubaity <165233122+A9ile@users.noreply.github.com>
2025-11-08 14:43:21 +0300

    Update answer.sql

--- answer.sql
+++ answer.sql
@@ -1,1 +1,2 @@
--- ADD YOUR SQL QUERY HERE
+select t.investor_id , s.sector_name, round((t.no_of_shares * 100.0 / sum(t.no_of_shares) over ( partition by t.investor_id)),2) as share_percentage from investor_transaction t 
+join sectors s on t.sector_id = s.sector_id order by t.investor_id, share_percetage desc;
